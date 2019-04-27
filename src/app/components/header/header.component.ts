2513aee565cff4f0cacad33cd7708e6e061b27f3
zmichgen <zmichgen@gmail.com>
2019-04-27 21:38:38 +0300

    feat: add debounceSubject in search function

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ApiService } from '../../services/api.service';
+import { Subject } from 'rxjs';
+import { debounceTime, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
@@ -9,13 +11,27 @@ import { ApiService } from '../../services/api.service';
 export class HeaderComponent implements OnInit {
   search: string;
   data = 'data';
+  private debounceSubject = new Subject<string>();
+
   constructor(private api: ApiService) { }
 
   ngOnInit() {
+    this.debounceSubject
+      .pipe(
+        filter((value) => !value.length || value.length >= 3),
+        debounceTime(1000)
+      ).subscribe((search) => {
+        this.change(search);
+      });
   }
 
-  change() {
-    this.api.doChange(this.search);
+  change(search: string): void {
+    this.api.doChange(search);
+  }
+
+  doChange(search: string): void {
+    this.debounceSubject.next(search);
+
   }
 
 }
